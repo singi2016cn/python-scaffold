dac45edd696ec8c46977a5593ec80433b3fe4447
singi <787575327@qq.com>
2018-10-14 22:24:30 +0800

    init

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+# hello world
+# singi
+
+my_name = 'singi'
+print(my_name.title())
+print(my_name.lower())
+print(my_name.upper())
+print(my_name)
---
+++ list.py
@@ -0,0 +1,9 @@
+guests = ['lishuting','qinyi']
+guests.append('qinjiuyou')
+guests.insert(1,'qinyongjie')
+for guest in guests:
+    print(guest)
+
+numbers = [i**2 for i in range(1,10)]
+for number in numbers:
+    print(number)
